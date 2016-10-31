ad7ee30f49f0573defe69cd0d1b9ad003378f1b8
michaelwoodguitar <michaelwoodguitar@gmail.com>
2016-10-31 14:45:17 +0000

    Required libraries build 1

---
+++ required.R
@@ -0,0 +1,13 @@
+# required libraries
+library(xlsx)
+library(knitr)
+library(stringi)
+library(lubridate)
+library(lhs)
+library(tictoc)
+library(EMD) 
+library(hht)
+library(BMS)
+library(GA)
+
+# functions
